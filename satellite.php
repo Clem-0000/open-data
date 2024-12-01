c4db242283146a1a5e0fc94eff3e897872e7f78a
Clément <clemtoon78700.bri@gmail.com>
2024-12-01 21:41:50 +0100

    Clean : remove deprecated/unused code and files

--- satellite.php
+++
@@ -1,9 +0,0 @@
-<?php
-$norad_id = $_GET['norad_id'];
-header('Content-Type: application/json');
-// $urlsatellite = "https://api.n2yo.com/rest/v1/satellite/tle/20436&apiKey=U9J3VY-5D8CUZ-7GNUDL-5CR1";
-$urlsatellite = "https://api.n2yo.com/rest/v1/satellite/tle/" . $norad_id . "&apiKey=U9J3VY-5D8CUZ-7GNUDL-5CR1";
-$contentsatellite = file_get_contents($urlsatellite);
-$jsonsatellite = json_decode($contentsatellite, true);
-
-echo json_encode($jsonsatellite);
